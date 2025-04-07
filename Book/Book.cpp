c61da7c80c22d84c7cdc532e7d12fc8db5285f57
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-04-07 16:04:16 -0500

    create Book class

---
+++ Book/Book.cpp
@@ -0,0 +1,34 @@
+#include "Book.h"
+
+Book::Book(string author, string title, string genre, int numPages)
+{
+    this->author = author;
+    this->title = title;
+    this->genre = genre;
+    this->numPages = numPages;
+}
+
+string Book::getAuthor() const
+{
+    return author;
+}
+
+string Book::getTitle() const
+{
+    return title;
+}
+string Book::getGenre() const
+{
+    return genre;
+}
+
+int Book::getNumPages() const
+{
+    return numPages;
+}
+
+void Book::printBookDetails() const
+{
+    cout << title << " by " << author << " has " << numPages << " pages, and its genre is " << genre << ".\n";
+
+}
