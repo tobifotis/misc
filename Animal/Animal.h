55884d2b70c16f005a3bcf0c4255c391d51db6b6
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-04-08 18:07:29 -0500

    Add Animal class and its implementation

---
+++ Animal/Animal.h
@@ -0,0 +1,22 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <string>
+using namespace std;
+
+class Animal
+{
+private:
+    string name;
+    double weight;
+
+public:
+    Animal(string name, double weight);
+    string getName() const;
+    void setName(string name);
+    double getWeight() const;
+    void setWeight(double weight);
+    string makeNoise() const;
+};
+
+#endif
