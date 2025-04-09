87f05fb3e6575e7aa48730b577b1600e42056e88
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-04-08 20:15:43 -0500

    Work on Enumeration

---
+++ Enum/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        STANDING
+    };
+
+    Direction myDirection = UP;
+    cout << myDirection << endl;
+
+    if (myDirection == UP)
+    {
+        cout << "up!" << endl;
+    }
+    else if(myDirection == DOWN)
+    {
+        cout << "down!" << endl;
+
+    }
+    else if(myDirection == LEFT)
+    {
+        cout << "left!" << endl;
+
+    }
+    else if(myDirection == RIGHT)
+    {
+        cout << "right!" << endl;
+
+    }
+    else if(myDirection == STANDING)
+    {
+        cout << "still standing!" << endl;
+
+    }
+
+
+
+    return 0;
+}
